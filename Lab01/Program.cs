aabc0c60a8b762b3daa7cde272c7d773748301fe
CarlSingletonIII <48929030+CarlSingletonIII@users.noreply.github.com>
2019-03-25 17:44:33 -0400

    Add files via upload

---
+++ Lab01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World from Carl Singleton!");
+
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+    }
+}
